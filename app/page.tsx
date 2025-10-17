203fde57a2013ff6456bc65d810a2f6fdda16dca
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-10-17 18:11:45 +0500

    Navbar finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
+import Navbar from "@/app/components/Navbar";
+
 export default function Home() {
   return (
       <div>
-        Hello Bekzod
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
+          <div className={"  w-full bg-red-500 h-80"}>
+
+          </div>
       </div>
   );
 }
