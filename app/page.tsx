74fd52edb66cf3d132fd9d174948072fd2a81561
Bekzod <bekzodmuqimovcse@gmail.com>
2025-10-26 17:29:50 +0500

    Catalogproduct

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Carousel from "@/app/components/Carousel";
 import Benefits from "@/app/components/Benefits";
 import Callretail from "@/app/components/Callretail";
+import Catalogproduct from "@/app/components/Catalogproduct";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <Carousel />
         <Benefits />
         <Callretail/>
+        <Catalogproduct/>
       </div>
     </div>
   );
