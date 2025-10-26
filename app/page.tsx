f9331501bf9ae0ae8d4ae06debd21aec7558718d
Bekzod <bekzodmuqimovcse@gmail.com>
2025-10-26 12:20:33 +0500

    Callretail setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Carousel from "@/app/components/Carousel";
 import Benefits from "@/app/components/Benefits";
+import Callretail from "@/app/components/Callretail";
+
 
 export default function Home() {
   return (
-      <div className={"mt-18 lg:mt-34"}>
-          <div>
-              <Carousel/>
-              <Benefits/>
-          </div>
+    <div className="mt-18 lg:mt-34">
+      <div>
+        <Carousel />
+        <Benefits />
+        <Callretail/>
       </div>
+    </div>
   );
 }
