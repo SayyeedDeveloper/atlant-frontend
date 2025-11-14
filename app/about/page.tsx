99a6757fb4a76c9f025e37eaab1c750515720fc3
Sayyeed <tmuhammadsayyid11@gmail.com>
2025-11-14 17:23:54 +0500

    about fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
-import Carousel from "@/app/components/Carousel";
+import AccordionAbout from "@/app/about/components/AccordionAbout";
 
 export default function Home() {
     return (
         <div className={"mt-18 lg:mt-34"}>
-            About
+            <AccordionAbout/>
         </div>
     );
 }
